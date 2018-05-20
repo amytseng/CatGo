6b320dde72db034f200a4ae876fbbf486d37abdb
Tseng Amy <amy.tseng@positivegrid.com>
2018-05-20 20:54:47 +0800

    amytseng:Add user

--- user/view.py
+++ user/view.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
+from user.models import User
 
 user_page = Blueprint('user_page', __name__)
 
 @user_page.route('/login')
 def login():
-    return "This is login page"
+    user = User(name='amytseng', password='1234', email='aammyytseng@gmail.com')
+    user.save()
+    return "Hello, {}!".format(user.name)
